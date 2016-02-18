f320bfbd569855b247dc950d951542a4c4d24bf1
Justin McElhaney <mcelhajd@mail.uc.edu>
2016-02-18 15:55:57 -0500

    Initial
    Push for Lab 2

---
+++ MapboxAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/android/testapp/MainActivity.java
@@ -0,0 +1,227 @@
+package com.mapbox.mapboxsdk.android.testapp;
+
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.InputType;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+	private DrawerLayout          mDrawerLayout;
+	private ActionBarDrawerToggle mDrawerToggle;
+	private NavigationView        mNavigationView;
+	private Menu                  testFragmentNames;
+	private int selectedFragmentIndex = 0;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		/*
+		   MapView.setDebugMode(true); //make sure to call this before the view is created!
+           */
+		setContentView(R.layout.activity_main);
+
+
+		mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+		mNavigationView = (NavigationView) findViewById(R.id.navigation_view);
+		mNavigationView.setNavigationItemSelectedListener(this);
+
+		// Set the adapter for the list view
+		testFragmentNames = mNavigationView.getMenu();
+		int i = 0;
+		testFragmentNames.add(Menu.NONE, i++, Menu.NONE, getString(R.string.navigation)); // Added this for Module 1
+		testFragmentNames.add(Menu.NONE, i++, Menu.NONE, getString(R.string.mainTestMap));
+		testFragmentNames.add(Menu.NONE, i++, Menu.NONE, getString(R.string.alternateTestMap));
+		testFragmentNames.add(Menu.NONE, i++, Menu.NONE, getString(R.string.markersTestMap));
+		testFragmentNames.add(Menu.NONE, i++, Menu.NONE, getString(R.string.itemizedOverlayTestMap));
+		testFragmentNames.add(Menu.NONE, i++, Menu.NONE, getString(R.string.localGeoJSONTestMap));
+		testFragmentNames.add(Menu.NONE, i++, Menu.NONE, getString(R.string.localOSMTestMap));
+		testFragmentNames.add(Menu.NONE, i++, Menu.NONE, getString(R.string.diskCacheDisabledTestMap));
+		testFragmentNames.add(Menu.NONE, i++, Menu.NONE, getString(R.string.offlineCacheTestMap));
+		testFragmentNames.add(Menu.NONE, i++, Menu.NONE, getString(R.string.programmaticTestMap));
+		testFragmentNames.add(Menu.NONE, i++, Menu.NONE, getString(R.string.webSourceTileTestMap));
+		testFragmentNames.add(Menu.NONE, i++, Menu.NONE, getString(R.string.locateMeTestMap));
+		testFragmentNames.add(Menu.NONE, i++, Menu.NONE, getString(R.string.pathTestMap));
+		testFragmentNames.add(Menu.NONE, i++, Menu.NONE, getString(R.string.bingTestMap));
+		testFragmentNames.add(Menu.NONE, i++, Menu.NONE, getString(R.string.saveMapOfflineTestMap));
+		testFragmentNames.add(Menu.NONE, i++, Menu.NONE, getString(R.string.tapForUTFGridTestMap));
+		testFragmentNames.add(Menu.NONE, i++, Menu.NONE, getString(R.string.customMarkerTestMap));
+		testFragmentNames.add(Menu.NONE, i++, Menu.NONE, getString(R.string.rotatedMapTestMap));
+		testFragmentNames.add(Menu.NONE, i++, Menu.NONE, getString(R.string.clusteredMarkersTestMap));
+		testFragmentNames.add(Menu.NONE, i++, Menu.NONE, getString(R.string.mbTilesTestMap));
+        testFragmentNames.add(Menu.NONE, i++, Menu.NONE, getString(R.string.draggableMarkersTestMap));
+		//testFragmentNames.add(Menu.NONE, i, Menu.NONE, getString(R.string.userAddressFragment));
+		// Set the drawer toggle as the DrawerListener
+		final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+		mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.navigationdrawer_open, R.string.navigationdrawer_close);
+		mDrawerLayout.setDrawerListener(mDrawerToggle);
+		setSupportActionBar(toolbar);
+
+		// Set MainTestFragment
+		selectItem(0);
+	}
+
+	public void editText(){
+
+	}
+
+	public void submitClicked(View view) {  // #1: Action from clicking the Submit button, this launches UserAddressFragment
+
+		EditText edText1 = (EditText) findViewById(R.id.editText);
+		//edText1.setInputType(InputType.TYPE_CLASS_TEXT);
+
+		String str = edText1.getText().toString();
+		Fragment fragment;
+		fragment = new userAddressFragment();
+		Bundle args = new Bundle();  				//Passes in the user address to UserAddressFragment
+		args.putString("address",str);
+		fragment.setArguments(args);
+		FragmentManager fragmentManager = getSupportFragmentManager();
+		fragmentManager.beginTransaction()
+				.replace(R.id.content_frame, fragment)
+				.commit();
+		/*
+		Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(str));
+		startActivity(browserIntent);*/
+
+	}
+
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState) {
+		super.onPostCreate(savedInstanceState);
+		mDrawerToggle.syncState();
+	}
+
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+		mDrawerToggle.onConfigurationChanged(newConfig);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		return mDrawerToggle.onOptionsItemSelected(item);
+	}
+
+	/**
+	 * Swaps fragments in the main content view
+	 */
+	private void selectItem(int position) {
+		final MenuItem menuItem = mNavigationView.getMenu().findItem(position);
+		setTitle(menuItem.getTitle());
+
+		selectedFragmentIndex = position;
+		// Create a new fragment and specify the planet to show based on position
+		Fragment fragment;
+
+		switch (position) {
+			case 0:
+				fragment = new NavigationFragment();    ///Creates a new fragment when clicked
+				break;
+			case 1:
+				fragment = new MainTestFragment();
+				break;
+			case 2:
+				fragment = new AlternateMapTestFragment();
+				break;
+			case 3:
+				fragment = new MarkersTestFragment();
+				break;
+			case 4:
+				fragment = new ItemizedIconOverlayTestFragment();
+				break;
+			case 5:
+				fragment = new LocalGeoJSONTestFragment();
+				break;
+			case 6:
+				fragment = new LocalOSMTestFragment();
+				break;
+			case 7:
+				fragment = new DiskCacheDisabledTestFragment();
+				break;
+			case 8:
+				fragment = new OfflineCacheTestFragment();
+				break;
+			case 9:
+				fragment = new ProgrammaticTestFragment();
+				break;
+			case 10:
+				fragment = new WebSourceTileTestFragment();
+				break;
+			case 11:
+				fragment = new LocateMeTestFragment();
+				break;
+			case 12:
+				fragment = new PathTestFragment();
+				break;
+			case 13:
+				fragment = new BingTileTestFragment();
+				break;
+			case 14:
+				fragment = new SaveMapOfflineTestFragment();
+				break;
+			case 15:
+				fragment = new TapForUTFGridTestFragment();
+				break;
+			case 16:
+				fragment = new CustomMarkerTestFragment();
+				break;
+			case 17:
+				fragment = new RotatedMapTestFragment();
+				break;
+			case 18:
+				fragment = new ClusteredMarkersTestFragment();
+				break;
+			case 19:
+				fragment = new MBTilesTestFragment();
+				break;
+            case 20:
+                fragment = new DraggableMarkersTestFragment();
+                break;
+			default:
+				fragment = new MainTestFragment();
+				break;
+		}
+
+		// Insert the fragment by replacing any existing fragment
+		FragmentManager fragmentManager = getSupportFragmentManager();
+		fragmentManager.beginTransaction()
+				.replace(R.id.content_frame, fragment)
+				.commit();
+
+		mDrawerLayout.closeDrawer(mNavigationView);
+	}
+
+	@Override
+	public void setTitle(CharSequence title) {
+		getSupportActionBar().setTitle(title);
+	}
+
+	@Override
+	public boolean onNavigationItemSelected(final MenuItem menuItem) {
+		selectItem(menuItem.getItemId());
+		return true;
+	}
+
+	@Override
+	public void onBackPressed() {
+		if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+			mDrawerLayout.closeDrawer(GravityCompat.START);
+		} else {
+			super.onBackPressed();
+		}
+	}
+}
---
+++ MapboxAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/android/testapp/userAddressFragment.java
@@ -0,0 +1,172 @@
+package com.mapbox.mapboxsdk.android.testapp;
+    import android.location.Address;
+    import android.location.Geocoder;
+    import android.os.Bundle;
+    import android.support.v4.app.Fragment;
+    import android.support.v4.app.FragmentManager;
+    import android.view.LayoutInflater;
+    import android.view.MotionEvent;
+    import android.view.View;
+    import android.view.ViewGroup;
+    import android.widget.TextView;
+
+    import com.mapbox.mapboxsdk.geometry.LatLng;
+    import com.mapbox.mapboxsdk.overlay.Marker;
+    import com.mapbox.mapboxsdk.views.InfoWindow;
+    import com.mapbox.mapboxsdk.views.MapView;
+
+    import java.util.List;
+
+
+    /**
+     * A simple {@link Fragment} subclass.
+     * Activities that contain this fragment must implement the
+     * {@link userAddressFragment.OnFragmentInteractionListener} interface
+     * to handle interaction events.
+     * Use the {@link userAddressFragment#newInstance} factory method to
+     * create an instance of this fragment.
+     */
+    public class userAddressFragment extends Fragment {
+        // TODO: Rename parameter arguments, choose names that match
+        // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+        private static final String ARG_PARAM1 = "param1";
+        private static final String ARG_PARAM2 = "param2";
+
+        // TODO: Rename and change types of parameters
+        private String addressKey;
+        private String addressValue;
+
+
+        public userAddressFragment() {
+            // Required empty public constructor
+        }
+
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment userAddressFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+    /*public static userAddressFragment newInstance(String addressKey, String addressValue) {
+        userAddressFragment fragment = new userAddressFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }*/
+
+
+        private MapView mapView;
+
+// This is the custom info window that launches the address when clicking the marker as well as launches the new fragment on click
+    public class addressWindow extends InfoWindow {
+        String address = getArguments().getString("address");
+        public addressWindow(MapView mv) {
+            super(R.layout.infowindow_custom, mv);
+
+            setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    if (event.getAction() == MotionEvent.ACTION_UP) {
+                        // Demonstrate custom onTouch()
+                        setOnTouchListener(this);
+
+                        Fragment fragment;
+                        fragment = new ContactManagerFragment();
+                        Bundle args = new Bundle();
+                        args.putString("address",address);
+                        fragment.setArguments(args);
+                        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                        fragmentManager.beginTransaction()
+                                .replace(R.id.content_frame, fragment)
+                                .commit();
+
+                        // Still close the InfoWindow though
+                        close();
+                    }
+
+                    // Return true as we're done processing this event
+                    return true;
+                }
+            });
+        }
+
+        /**
+         * Dynamically set the content in the CustomInfoWindow
+         * @param overlayItem The tapped Marker
+         */
+        @Override
+        public void onOpen(Marker overlayItem) {
+            String title = overlayItem.getTitle();
+            ((TextView) mView.findViewById(R.id.customTooltip_title)).setText(title);
+        }
+    }
+        //This sets up the map with the user address after it is converted to lat and long coordinates
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_localgeojson, container, false);
+
+
+        // Setup Map
+        mapView = (MapView) view.findViewById(R.id.localGeoJSONMapView);
+        // mapView.goToUserLocation(true);
+        mapView.setCenter(new LatLng(47.668780, -122.387883));
+        mapView.setZoom(14);
+        // mapView.getUserLocationEnabled();
+
+        String address = getArguments().getString("address");
+
+        Geocoder coder = new Geocoder(userAddressFragment.this.getActivity());
+        List<Address> addresslist;
+        LatLng p1 = null;
+        double p1Lat = 0;
+        double p1Long = 0;
+
+        try {
+            addresslist = coder.getFromLocationName(address, 5);
+            if (addresslist == null) {
+                return null;
+            }
+            Address location = addresslist.get(0);
+            p1Lat = location.getLatitude();
+            p1Long = location.getLongitude();
+
+            p1 = new LatLng(location.getLatitude(), location.getLongitude());
+
+        } catch (Exception ex) {
+
+            ex.printStackTrace();
+        }
+        //This adds the markers and centers the map on its location
+
+        mapView.setCenter(new LatLng(p1));
+        Marker Home = new Marker(mapView, address + "  Navigate Here", "", new LatLng(p1));
+
+        Home.setToolTip(new addressWindow(mapView));
+
+
+        mapView.addMarker(Home);
+
+
+        String searchUrl = "https://api.mapbox.com/geocoding/v5/mapbox.places/" + p1Long + "," + p1Lat + ".json?types=region&access_token=&lt;your%20access%20token&gt;";
+        return view;
+
+    }
+    @Override
+    public void onResume()
+        {
+        super.onResume();
+
+
+
+
+
+
+
+    }
+
+
+}
